7f6e14004743993cd4f622dc26dc11cd9d46de7f
marlion01 <fantand.greeeen.mass@gmail.com>
2024-01-03 17:32:40 +0900

    first commit

---
+++ bin/freezed_trainning.dart
@@ -0,0 +1,5 @@
+import 'package:freezed_trainning/freezed_trainning.dart' as freezed_trainning;
+
+void main(List<String> arguments) {
+  print('Hello world: ${freezed_trainning.calculate()}!');
+}
---
+++ test/freezed_trainning_test.dart
@@ -0,0 +1,8 @@
+import 'package:freezed_trainning/freezed_trainning.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
